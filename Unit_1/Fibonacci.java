2243d2091bd00d8391b616e7c4df77297c4e2362
Lee Mirsky <lee.mirsky@marlborough.org>
2022-09-13 11:49:29 -0700

    9-13-22

---
+++ Unit_1/Fibonacci.java
@@ -0,0 +1,17 @@
+public class Fibonacci
+{
+    public static void main(String[] args)
+    {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+
+        for (int i=0; i<10; i++)
+        {
+
+          System.out.println();
+
+        }
+
+    }
+}
---
+++ Unit_1/Geometric.java
@@ -0,0 +1,13 @@
+public class Geometric
+{
+    public static void main(String[] args)
+    {
+        int n = 3;
+
+        for (int i=0; i<27; i++)
+        {
+          System.out.println(n);
+          n*=3;
+        }
+    }
+}
